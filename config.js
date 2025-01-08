dffa98b99a460b2c822f6f4d7f04afcf8304a603
Heer <heerlicious1@gmail.com>
2025-01-09 03:04:26 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "ATUyFDwJ#07LmjsKNi1kP8sDQNF0jh_WFvhYNdAOlEV6hTSjqe4M",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/n5vvij.jpg",
-SUDO_NB: process.env.SUDO_NB || "923259578429",
+SUDO_NB: process.env.SUDO_NB || "923708057748",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
